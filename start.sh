0282e538a7979b0c6be6e161df3faca2b00e3217
gisman <gisman@gmail.com>
2025-04-23 02:54:16 +0900

    .gitignore에서 start.sh 파일 제외 및 start.sh 스크립트 추가

---
+++ start.sh
@@ -0,0 +1,14 @@
+source venv/bin/activate
+
+lsof -ti:36379 | xargs kill -9
+
+echo 'starting'
+
+# echo | nohup python src/main.py --reload >> log/bigcache.log &
+echo | nohup python src/main.py --port=36379 --db_path=/disk/ssd2t/bigcache  >> log/bigcache.log &
+
+sleep 1
+
+echo 'started'
+
+
